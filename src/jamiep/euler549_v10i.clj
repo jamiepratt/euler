61e9591c2d0161a07c5ff200328bf8868ab46edc
James Pratt <me@jamiep.org>
2023-05-16 13:01:11 +0200

    version with arrays now working

--- src/jamiep/euler549_v10i.clj
+++ src/jamiep/euler549_v10i.clj
@@ -1,9 +1,11 @@
 (ns jamiep.euler549-v10i
   (:require [hyperfiddle.rcf :refer [tests]]))
 
+(set! *warn-on-reflection* true)
+
 ;; version of v10 using mutation in place
 
-(defn array-of-largest-prime-factors-for-all-int-to [^long max-n]
+(defn array-of-largest-prime-factors-for-all-int-to [max-n]
   (let [pfs (int-array (range 0 (inc max-n)))] 
     (loop [n 2]
       (if (<= n max-n)
@@ -11,7 +13,7 @@
           (when (= n (get pfs n))
             (loop [multiple (* n 2)]
               (when (<= multiple  max-n)
-                (aset pfs multiple n)
+                (aset-int pfs multiple n)
                 (recur (+ multiple n))))); found new prime
           (recur (inc n)))
         pfs))))
@@ -53,58 +55,51 @@
     (next-integer-with-factor p prev-m)))
 
 (defn array-of-smallest-m-till' [^long q]
-  (let [pfs (array-of-largest-prime-factors-for-all-int-to q)
+  (let [^ints pfs (array-of-largest-prime-factors-for-all-int-to q)
         s (int-array (inc q))] ; initialised to zero by default
     (loop [x 2]
       (if (<= x q)
         (let [p (aget pfs x)]
           (if (= p x)
-            (aset s x x) ; prime so smallest-m = p = x
+            (aset-int s x x) ; prime so smallest-m = p = x
             (let [prev-m (aget s (quot x p))]
               (if (< prev-m p)
-                (aset s x p)
-                (aset s x (smallest-m-where-x-divides-m! prev-m p x)))))
+                (aset-int s x p)
+                (aset-int s x (smallest-m-where-x-divides-m! prev-m p x)))))
           (recur (inc x)))
         s))))
 
-(defn array-of-smallest-m-till [q]
-  (let [pfs (array-of-largest-prime-factors-for-all-int-to q)]
-    (persistent!
-     (reduce
-      (fn [s x]
-        (let [p (aget pfs x)]
-          (if (= p x)
-            (conj! s p) ; prime so m = p
-            (let [prev-m (get s (quot x p))]
-              (if (< prev-m p)
-                (conj! s p)
-                (conj! s (smallest-m-where-x-divides-m! prev-m p x)))))))
-      (transient [0 0])
-      (range 2 (inc q))))))
+;; (defn array-of-smallest-m-till [q]
+;;   (let [pfs (array-of-largest-prime-factors-for-all-int-to q)]
+;;     (persistent!
+;;      (reduce
+;;       (fn [s x]
+;;         (let [p (get pfs x)]
+;;           (if (= p x)
+;;             (conj! s p) ; prime so m = p
+;;             (let [prev-m (get s (quot x p))]
+;;               (if (< prev-m p)
+;;                 (conj! s p)
+;;                 (conj! s (smallest-m-where-x-divides-m! prev-m p x)))))))
+;;       (transient [0 0])
+;;       (range 2 (inc q))))))
 
-(tests
- (vec (array-of-smallest-m-till 10)) := [0 0 2 3 4 5 3 7 4 6 5])
+;; (tests
+;;  (vec (array-of-smallest-m-till 10)) := [0 0 2 3 4 5 3 7 4 6 5])
 
 (defn sum-of-smallest-m-till' [^long q]
-  (let [s (array-of-smallest-m-till q)]
+  (let [^ints s (array-of-smallest-m-till' q)]
     (loop [x 2
            sum 0]
       (if (<= x q)
         (recur (inc x) (+ (aget s x) sum))
         sum))))
 
-(defn sum-of-smallest-m-till [q]
-  (let [s (array-of-smallest-m-till q)]
-    (apply + s)))
-
-(tests (sum-of-smallest-m-till 100) := 2012)
-
-;; ;; (defn -main [& _args]
-;; ;;   (doall (map #(let [n-to %
-;; ;;                      p (Math/pow 10 %)]
-;; ;;                  (println (str "sum of series to 1e" n-to))
-;; ;;                  (println (time (sum-of-smallest-m-till p))))
-;; ;;               (range 2 9))))
+;; (defn sum-of-smallest-m-till [q]
+;;   (let [s (array-of-smallest-m-till q)]
+;;     (apply + s)))
 
 
-;; #_(time (sum-of-smallest-m-till (int (Math/pow 10 6))))
+;; (tests (sum-of-smallest-m-till 100) := 2012)
+
+(tests (sum-of-smallest-m-till' 100) := 2012)
